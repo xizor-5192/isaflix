c0b91f8e6ca14a4f72f0b4f755a03cf3307fca91
xizor-5192 <fmpaiola@hotmail.com>
2020-07-30 21:41:49 -0300

    Commit

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
-//import ButtonLink from '../components/ButtonLink';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
-                <img className="Logo" src={Logo} alt="Logo da IsaFlix" />
-            </a>
-            <Button as="a" className="ButtonLink" href="/">
+            <Link to="/">
+                <img className="Logo" src={Logo} alt="IsaFlix logo" />
+            </Link>
+
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
